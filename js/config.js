c939a128211f1c9fcdaaa7109e053dcbb38119bb
Luiz Gabriel <dev.luizg@gmail.com>
2026-04-09 19:55:27 -0300

    refactor: update service worker caching strategy and remove unnecessary config.js file

---
+++ js/config.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = 'https://ougloyumvebmqfegbvwy.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_fUBk2OnSnJhgWaC6MsVROQ_tgBuWbPE';
--- sw.js
+++ sw.js
@@ -1,16 +1,13 @@
 const CACHE_NAME = 'ocorrencias-v1';
-const urlsToCache = ['/', '/index.html'];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+    self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
     );
 });
